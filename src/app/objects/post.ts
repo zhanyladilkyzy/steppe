9086b76aef7c5e89e895baae198e43c695c96a12
zhanyladilkyzy <52228972+zhanyladilkyzy@users.noreply.github.com>
2021-03-17 23:05:05 +0600

    deleting extra fields

--- src/app/objects/post.ts
+++ src/app/objects/post.ts
@@ -3,14 +3,11 @@ export class Post {
   public text: string;
   public date: string;
   public author: string;
-  // public post: Post[] = [];
-
 
   constructor(title: string, text: string, date: string, author: string) {
     this.title = title;
     this.text = text;
     this.date = date;
     this.author = author;
-    // this.post = [];
   }
 }
