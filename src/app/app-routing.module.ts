b51962b818d130db533b6664cd21c6227c8bf1f1
zhanyladilkyzy <52228972+zhanyladilkyzy@users.noreply.github.com>
2021-04-09 18:29:42 +0600

    Add CanLoad Guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,7 @@ import { ArticleComponent } from './article/article.component';
 import { ArticleCanDeactivateGuardService } from './services/article-can-deactivate-guard.service'
 import {PostNotFoundComponent} from "./post-not-found.component";
 import {PostDetailsGuardService} from "./services/post-details-guard.service";
+import {PreferencesCheckGuard} from "./services/preferences-check.guard";
 
 
 const routes: Routes = [{path: 'navpage', component: NavpageComponent},
@@ -14,6 +15,9 @@ const routes: Routes = [{path: 'navpage', component: NavpageComponent},
                         {path:'article', component: ArticleComponent, canDeactivate: [ArticleCanDeactivateGuardService]},
                         {path: 'notfound', component: PostNotFoundComponent},
                         {path:'', redirectTo:'homepage',pathMatch:'full'},
+                        { path: 'preferences',
+                          canLoad: [PreferencesCheckGuard],
+                          loadChildren: () => import('./preferences/preferences.module').then(m => m.PreferencesModule) },
                         {path: '**', component: ErrorComponent}
                       ];
 
