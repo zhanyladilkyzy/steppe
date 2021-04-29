f49622fc415f02ec6a93c0d8fcfbe970ee8e1766
zhanyladilkyzy <52228972+zhanyladilkyzy@users.noreply.github.com>
2021-04-29 07:22:15 +0600

    Adding new Read Article component

---
+++ src/app/read-article/read-article.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-read-article',
+  templateUrl: './read-article.component.html',
+  styleUrls: ['./read-article.component.scss']
+})
+export class ReadArticleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
