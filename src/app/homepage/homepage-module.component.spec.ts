c2be0542a5e60e3d0c556e508c0a981ec826d5d1
zhanyladilkyzy <52228972+zhanyladilkyzy@users.noreply.github.com>
2021-04-29 07:30:08 +0600

    renaming HomePage component

--- src/app/homepage/homepage-module.component.spec.ts
+++ src/app/homepage/homepage-module.component.spec.ts
@@ -1,20 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { HomepageComponent } from './homepage.component';
+import { HomepageModuleComponent } from './homepage-module.component';
 
 describe('HomepageComponent', () => {
-  let component: HomepageComponent;
-  let fixture: ComponentFixture<HomepageComponent>;
+  let component: HomepageModuleComponent;
+  let fixture: ComponentFixture<HomepageModuleComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ HomepageComponent ]
+      declarations: [ HomepageModuleComponent ]
     })
     .compileComponents();
   });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(HomepageComponent);
+    fixture = TestBed.createComponent(HomepageModuleComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
