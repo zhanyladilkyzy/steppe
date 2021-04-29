a6ed52460a9c5b6e562bb45bf5fc47451bd5d041
zhanyladilkyzy <52228972+zhanyladilkyzy@users.noreply.github.com>
2021-04-29 07:25:21 +0600

    updating routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,24 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { NavpageComponent } from './navpage/navpage.component';
-import { HomepageComponent } from './homepage/homepage.component';
+import { HomepageModuleComponent } from './homepage/homepage-module.component';
 import { ErrorComponent } from './error/error.component';
 import { ArticleComponent } from './article/article.component';
 import { ArticleCanDeactivateGuardService } from './services/article-can-deactivate-guard.service'
 import {PostNotFoundComponent} from "./post-not-found.component";
 import {PostDetailsGuardService} from "./services/post-details-guard.service";
 import {PreferencesCheckGuard} from "./services/preferences-check.guard";
+import {ReadArticleComponent} from "./read-article/read-article.component";
 
 
 const routes: Routes = [{path: 'navpage', component: NavpageComponent},
-                        {path:'homepage', component: HomepageComponent, canActivate: [PostDetailsGuardService]},
-                        {path:'article', component: ArticleComponent, canDeactivate: [ArticleCanDeactivateGuardService]},
-                        {path: 'notfound', component: PostNotFoundComponent},
+                        {path:'homepage', component: HomepageModuleComponent, canActivate: [PostDetailsGuardService]},
+                        // {path:'article', component: ArticleComponent, canDeactivate: [ArticleCanDeactivateGuardService]},
+                        // {path:'readarticle', component: ReadArticleComponent},
                         {path:'', redirectTo:'homepage',pathMatch:'full'},
                         { path: 'preferences',
                           canLoad: [PreferencesCheckGuard],
                           loadChildren: () => import('./preferences/preferences.module').then(m => m.PreferencesModule) },
-                        {path: '**', component: ErrorComponent}
+                        {path: '**', component: PostNotFoundComponent}
                       ];
 
 @NgModule({
