9dce58df51570cf647e3ea4d01cc77d7fbe560d6
zhanyladilkyzy <52228972+zhanyladilkyzy@users.noreply.github.com>
2021-04-29 07:24:10 +0600

    Updating for JSON service

--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {RestService} from "../rest.service";
+import {Post} from "../objects/post";
 
 @Component({
   selector: 'app-article',
@@ -6,10 +8,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./article.component.scss']
 })
 export class ArticleComponent implements OnInit {
+  title  = 'JSONServerApi';
+  constructor(private rs: RestService) { }
 
-  constructor() { }
+  columns  = ["Post ID", "Title", "Text", "Date", "Author"];
+
+ // index = ["id", "title", "text", "date", "author"];
+  index = [1,2,3,4,5]
+  posts: Post[] = [];
 
   ngOnInit(): void {
+    this.rs.getPosts().subscribe
+    (
+      (response)=>
+      {
+        this.posts = response;
+      },
+      (error)=>
+      {
+        console.log("ERROR: " + error);
+      }
+    )
   }
 
 }
